866513b28bab8398e9ba38111d18cd45cddf0885
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-02-06 11:56:54 -0300

    Use rectangle efficiently

--- game.py
+++ game.py
@@ -11,8 +11,8 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black') # text, AA, color
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
@@ -27,11 +27,10 @@ while True:
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (330, 50))
 
-    snail_x_pos -= 4
-    snail_rect = snail_surface.get_rect(midbottom = (snail_x_pos, 300))
-    screen.blit(snail_surface, snail_rect)
-    if snail_x_pos <= -50:
-        snail_x_pos = 800
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
     
     player_rect.left += 1
     screen.blit(player_surf, player_rect)
