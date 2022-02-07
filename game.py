b8c6086338e2a1335b222d329aa17570c06d86c5
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-02-07 10:59:03 -0300

    Center text using rectangle

--- game.py
+++ game.py
@@ -9,7 +9,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black') # text, AA, color
+
+score_surf = test_font.render('My game', False, 'Black') # text, AA, color
+score_rect = score_surf.get_rect(midtop = (400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (600, 300))
@@ -22,15 +24,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('collision')
     
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (330, 50))
+    screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    #    print('collision')
+    
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
     
     player_rect.left += 1
     screen.blit(player_surf, player_rect)
