744ceb95e1740df13124b80b152a927da2e60d4b
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-02-08 11:00:15 -0300

    Implement player's jump

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render('My game', False, 'Black') # text, AA, color
+score_surf = test_font.render('My game', False, (64, 64, 64)) # text, AA, color
 score_rect = score_surf.get_rect(midtop = (400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -18,18 +18,28 @@ snail_rect = snail_surf.get_rect(bottomright = (600, 300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos):
-        #         print('collision')
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                if player_rect.bottom >= 300:
+                    player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if player_rect.bottom >= 300:
+                    player_gravity = -20
     
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
     screen.blit(score_surf, score_rect)
 
     snail_rect.x -= 4
@@ -37,14 +47,10 @@ while True:
         snail_rect.left = 800
     screen.blit(snail_surf, snail_rect)
 
-    # if player_rect.colliderect(snail_rect):
-    #    print('collision')
-    
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
-    
-    player_rect.left += 1
+    player_gravity += 1
+    player_rect.y  += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
     screen.blit(player_surf, player_rect)
 
     pygame.display.update()
